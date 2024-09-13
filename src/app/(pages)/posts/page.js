fe4227add07b3822b31eb69e8161c18357ccc1aa
Gui-fga <222006777@aluno.unb.br>
2024-09-13 19:00:42 -0300

    fiz quase nada kk

--- src/app/(pages)/posts/page.js
+++ src/app/(pages)/posts/page.js
@@ -1,8 +1,12 @@
-export default function CriarPosts (){
-    return (
-      <div>
-        <h1>Crie sus postagem</h1>
-        <input class="p-2 w-96 text-black"type="text" placeholder="Email"/>
-        </div>
-  )
+export default function Login (){
+  return (
+    <div className="flex flex-col items-center justify-center h-screen"> 
+        <h1 class='text-2xl mb-10'>Crie sua postegem</h1>       
+        <form class="flex flex-col space-y-2">
+          <input class="p-2 w-96 text-black"type="text" placeholder="Título"/>
+          <input class="p-2 w-96 text-black" type="password" placeholder="Postagem"/>
+          <button class="bg-green-950 p-2 hover:bg-green-900"> Criar </button>
+      </form>
+  </div>
+)
 }
