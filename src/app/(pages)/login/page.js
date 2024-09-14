e9c49b2abf94a67e8e8c875cf7925d2dc23428b3
Rodrigo Ferreira <rodrigo.wendrel@protonmail.com>
2024-09-14 15:26:07 -0300

    função de login

--- src/app/(pages)/login/page.js
+++ src/app/(pages)/login/page.js
@@ -1,24 +1,48 @@
-export default function Login() {
-  const handleSubmit = (e) => {
-    const name = form.name.value;
-    // const email = e.target[1].value;
-    // const password = e.target[2].value;
+"use client";
+import { useRouter } from "next/navigation";
 
-    console.log('alô')
-    console.log(name);
+export default function Login() {
+  const router = useRouter();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    const { data, error } = await fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    }).then((res) => res.json());
+
+    if (error) {
+      alert(error.message);
+    }
+    alert("Login realizado com sucesso");
+    router.push("/");
+    console.log(data);
   };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl mb-10">Entre na sua conta</h1>
-      <form className="flex flex-col space-y-2" onSubmit={handleSubmit}>
-        <input className="p-2 w-96 text-black" type="text" placeholder="Email" />
+      <form className="flex flex-col space-y-2" onSubmit={handleLogin}>
+        <input
+          className="p-2 w-96 text-black"
+          type="text"
+          placeholder="Email"
+        />
         <input
           className="p-2 w-96 text-black"
           type="password"
           placeholder="Senha"
         />
-        <button className="bg-green-950 p-2 hover:bg-green-900"> Entrar </button>
+        <button type="submit" className="bg-green-950 p-2 hover:bg-green-900">
+          {" "}
+          Entrar{" "}
+        </button>
       </form>
     </div>
   );
--- src/app/(pages)/register/page.js
+++ src/app/(pages)/register/page.js
@@ -1,13 +1,50 @@
-export default function Register (){
-    return (
-      <div className="flex flex-col items-center justify-center h-screen"> 
-          <h1 className='text-2xl mb-10'>Crie sua conta</h1>       
-          <form className="flex flex-col space-y-2">
-            <input className="p-2 w-96 text-black"type="text" placeholder="Nome"/>
-            <input className="p-2 w-96 text-black"type="text" placeholder="Email"/>
-            <input className="p-2 w-96 text-black" type="password" placeholder="Senha"/>
-            <button className="bg-green-950 p-2 hover:bg-green-900"> Entrar </button>
-        </form>
+"use client";
+import { useRouter } from "next/navigation";
+
+export default function Register() {
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+
+    const data = await fetch("/api/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, password }),
+    }).then((res) => res.json());
+
+    if (data.error) {
+      alert(data.error);
+    }
+
+    alert("Conta criada com sucesso");
+    router.push("/login");
+
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl mb-10">Crie sua conta</h1>
+      <form className="flex flex-col space-y-2" onSubmit={handleSubmit}>
+        <input className="p-2 w-96 text-black" type="text" placeholder="Nome" />
+        <input
+          className="p-2 w-96 text-black"
+          type="text"
+          placeholder="Email"
+        />
+        <input
+          className="p-2 w-96 text-black"
+          type="password"
+          placeholder="Senha"
+        />
+        <button className="bg-green-950 p-2 hover:bg-green-900">Entrar</button>
+      </form>
     </div>
-  )
+  );
 }
